5e319d6e3e336b97189e3ffc1a500abed12d0061
Sean Tansey <sean.d.tansey@gmail.com>
2022-07-27 21:24:58 -0600

    adds send_page_view to ga

--- src/utils/google-analytics.js
+++ src/utils/google-analytics.js
@@ -4,6 +4,7 @@ export const trackPageView = (title, path) => {
     gtag('config', process.env.VUE_APP_GA_TRACKING_ID, {
         page_title: title,
         page_path: path,
-        page_location: window.location.href
+        page_location: window.location.href,
+        send_page_view: true
       })
 }
